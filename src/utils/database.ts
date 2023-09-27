f34969606d385b0a57e9a98e1211e02c21ced1de
Yusuf Demir <jamesyusuf36@gmail.com>
2023-09-27 22:06:05 +0300

    Code updated
    This project developed further in another repo. Those changes are updated in this repo.

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,13 +1,15 @@
 //Connect to postgresql
 import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "ims",
-    password: "1234",
-    port: 5432
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
 })
 
 export default pool;
